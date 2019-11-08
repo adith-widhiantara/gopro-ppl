ec595449b02ea5387eedd188b5c4d168747aec31
Adith Widhiantara <172410101108@cs.unej.ac.id>
2019-11-08 17:00:22 +0700

    2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,75 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('base.materialize')
 
-        <title>Laravel</title>
+@section('title', "GoPro")
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+@section('materialize')
+<head>
+  <link rel="stylesheet" href="{{ url('css/landingpage.css') }}">
+</head>
+<!-- Navbar -->
+<div class="navbar-fixed">
+  <nav class="transparent z-depth-0">
+    <div class="nav-wrapper container">
+      <a href="#" class="brand-logo right">Logo</a>
+      <ul id="nav-mobile" class="left hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">JavaScript</a></li>
+      </ul>
+    </div>
+  </nav>
+</div>
+<!-- End Navbar -->
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+<!-- Slider -->
+<div class="slider">
+    <ul class="slides">
+      <li>
+        <img src="{{ asset('img/bglanding.jpg') }}"> <!-- random image -->
+        <div class="caption center-align">
+          <h3>Bantu produktivitas perkebunan di Indonesia bersama GoPro</h3>
+          <h5 class="light grey-text text-lighten-3">GoPro</h5>
         </div>
-    </body>
-</html>
+      </li>
+      <li>
+        <img src="{{ asset('img/bglanding2.jpg') }}"> <!-- random image -->
+        <div class="caption right-align">
+          <h3>Bantu produktivitas perkebunan<br>di Indonesia bersama GoPro</h3>
+          <h5 class="light grey-text text-lighten-3">GoPro</h5>
+        </div>
+      </li>
+      <li>
+        <img src="{{ asset('img/bglanding3.jpg') }}"> <!-- random image -->
+        <div class="caption left-align">
+          <h3>Bantu produktivitas perkebunan di<br>Indonesia bersama GoPro</h3>
+          <h5 class="light grey-text text-lighten-3">GoPro</h5>
+        </div>
+      </li>
+      <li>
+        <img src="{{ asset('img/bglanding4.jpg') }}"> <!-- random image -->
+        <div class="caption center-align">
+          <h3>Bantu produktivitas perkebunan di Indonesia bersama GoPro</h3>
+          <h5 class="light grey-text text-lighten-3">GoPro</h5>
+        </div>
+      </li>
+    </ul>
+  </div>
+<!-- End Slider -->
+
+
+
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.slider');
+  var instances = M.Slider.init(elems, {
+    indicators: false,
+    height: 600
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems);
+  });
+</script>
+@endsection

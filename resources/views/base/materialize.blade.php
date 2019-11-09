f5223fd90c6e19576ccc907df0e98228e4827ff9
Adith Widhiantara <172410101108@cs.unej.ac.id>
2019-11-09 16:13:01 +0700

    3 lab

--- resources/views/base/materialize.blade.php
+++ resources/views/base/materialize.blade.php
@@ -5,6 +5,8 @@
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <!--Import materialize.css-->
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+      <!-- Font Awesome 4.7 -->
+      <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 
       <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
@@ -18,5 +20,6 @@
 
       <!--JavaScript at end of body for optimized loading-->
       <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+      <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
     </body>
   </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,16 +5,20 @@
 @section('materialize')
 <head>
   <link rel="stylesheet" href="{{ url('css/landingpage.css') }}">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Counter-Up/1.0.0/jquery.counterup.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.js"></script>
 </head>
 <!-- Navbar -->
 <div class="navbar-fixed">
-  <nav class="transparent z-depth-0">
+  <nav class="navnav">
     <div class="nav-wrapper container">
-      <a href="#" class="brand-logo right">Logo</a>
-      <ul id="nav-mobile" class="left hide-on-med-and-down">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">JavaScript</a></li>
+      <a href="#" class="brand-logo">GOPRO</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="#">Proyek Pendanaan</a></li>
+        <li><a href="#">Jadi Mitra</a></li>
+        <li><a href="#">Tentang Kami</a></li>
+        <li><a href="#">Masuk</a></li>
       </ul>
     </div>
   </nav>
@@ -22,7 +26,7 @@
 <!-- End Navbar -->
 
 <!-- Slider -->
-<div class="slider">
+<div style="z-index: -1" class="slider">
     <ul class="slides">
       <li>
         <img src="{{ asset('img/bglanding.jpg') }}"> <!-- random image -->
@@ -56,9 +60,95 @@
   </div>
 <!-- End Slider -->
 
+<!-- Rectangle -->
+<div style="z-index: 5" class="container middle">
+  <div class="counting-sec">
+    <div class="inner-width">
+
+      <div class="col coll">
+        <div class="num">
+          60
+        </div>
+        Petani Terdanai
+      </div>
+
+      <div class="hr"></div>
+
+      <div class="col coll">
+        <div class="num">
+          120
+        </div>
+        Jumlah Investor
+      </div>
+
+      <div class="hr"></div>
+
+      <div class="col coll">
+        <div class="inlin">
+          <div class="numm">
+            Rp
+          </div>
+          <div class="num">
+            180
+          </div>
+          <div class="numm">
+            JT
+          </div>
+        </div>
+        Dana Tersalurkan
+      </div>
+
+    </div>
+  </div>
+</div>
+<!-- End Rectangle -->
+
+<!-- Mulai Investasi -->
+<div style="z-index: -1" class="row bantupetani">
+    <div class="col s4 offset-s2">
+      <p>Bantu Petani perkebunan di Indonesia<br>untuk lebih produktif dalam menghasilkan<br>panen yang berkualitas.</p>
+      <a class="waves-effect waves-light btn green">Mulai Investasi</a>
+    </div>
+    <div class="col s4">
+      <img src="{{ asset('img/mulaiinvestasi.jpg') }}" alt="">
+    </div>
+</div>
+<!-- End Mulai Investasi -->
+
+<!-- Kisah Sukses -->
+<div class="kisahsukses">
+  <div class="container">
+    <div class="row">
+      <div class="col s4 offset-s2">
+        <img src="{{ asset('img/petani.jpg') }}" alt="">
+      </div>
+      <div class="col s4">
+        <div class="card warnacard">
+          <div class="card-content white-text">
+            <span class="card-title">Kisah Sukses</span>
+            <p>I am a very simple card. I am good at containing small bits of information.
+            I am convenient because I require little markup to use effectively.</p>
+          </div>
+      </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- End Kisah Sukses -->
+
 
 
 <script>
+$(document).ready(function() {
+        $(window).scroll(function() {
+          if($(this).scrollTop() > 50) {
+              $('.navnav').addClass('solid');
+          } else {
+              $('.navnav').removeClass('solid');
+          }
+        });
+});
+
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.slider');
   var instances = M.Slider.init(elems, {
@@ -71,5 +161,10 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.parallax');
     var instances = M.Parallax.init(elems);
   });
+
+$(".num").counterUp({
+  delay:10,
+  time:1000
+});
 </script>
 @endsection
